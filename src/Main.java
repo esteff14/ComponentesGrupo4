82413edccd1808593ff28439d122e602fd971012
Estefanía Sánchez <estefi2000ms2@gmail.com>
2024-01-24 20:43:28 -0500

    comentarios

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ import java.awt.*;
 public class Main {
     static JFrame frame = new JFrame("Inicio de Sesion"); //Se crea un objeto
     public static void main(String[] args) {
-        frame.setContentPane(new form1().inicio);
+        frame.setContentPane(new form1().inicio);// creacion de un objeto en el panel de contenido del frame inicio
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+        frame.setLocationRelativeTo(null); // centrar la ventana
+        frame.getContentPane().setBackground(Color.LIGHT_GRAY); //color al fondo de la ventana
         frame.setSize(400,400);
         frame.setVisible(true);
     }
--- src/form1.java
+++ src/form1.java
@@ -10,20 +10,23 @@ public class form1 {
     private JButton INGRESARButton;
     static JFrame frame2 = new JFrame("Pantalla");
     public form1() {
-        INGRESARButton.addActionListener(new ActionListener() {
+        INGRESARButton.addActionListener(new ActionListener() { //
             @Override
             public void actionPerformed(ActionEvent e) {
-                String usuario="Admin";
+                String usuario="Admin"; // Datos quemados para el inicio de sesion
                 String pass="admin";
-                if (usuarioField.getText().equals(usuario)&&String.valueOf(passwordField.getPassword()).equals(pass)){
-                    frame2.setContentPane(new form2().pantalla);
-                    frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    frame2.setLocationRelativeTo(Main.frame);
-                    frame2.setSize(400,400);
-                    frame2.setVisible(true);
+                if (usuarioField.getText().equals(usuario)&&String.valueOf(passwordField.getPassword()).equals(pass)){ //se compara con los datos quemados para poder
+                    // ingresar si coincide podrá acceder a la siguiente pantalla
+                    frame2.setContentPane(new form2().pantalla); //se crea un objeto de la clase form2
+                    frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // se establece para el cierre de pantalla
+                    frame2.setLocationRelativeTo(Main.frame); //se establece el posicionamiento de la pantalla al emerger
+                    //en este caso cerca del frame (inicio)
+                    frame2.setSize(400,400); //tamaño de la pantalla
+                    frame2.setVisible(true); //hace que la pantalla sea visible
                     Main.frame.dispose();
                 }else {
                     JOptionPane.showMessageDialog(null,"Ingrese las credenciales correctas!");
+                    //si las credenciales no son correctas enviará un mensaje de advertencia
                 }
             }
         });
